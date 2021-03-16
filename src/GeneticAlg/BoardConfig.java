21df8197abf860b23201c93c2415b281285e5278
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2021-03-16 17:45:36 +0100

    Add some constant variables for future

--- src/GeneticAlg/BoardConfig.java
+++ src/GeneticAlg/BoardConfig.java
@@ -16,6 +16,7 @@ public class BoardConfig {
     public final static int OUT_OF_BOUNDS_WEIGHT = 10;
     public final static int SEGMENT_WEIGHT = 1;
     public final static int LENGTH_WEIGHT =1;
+    public final static int MAX_MUTATION_FORCE = 5;
 
 
     public BoardConfig(String filename) throws FileNotFoundException {
